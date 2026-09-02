e3f15bd5baf4ad9ff2e85bc25f4dba8a7a4d27bf
H2o86 <66200730+H2o86@users.noreply.github.com>
2026-09-02 23:37:57 +0700

    fix: bundle Qt platform plugins (qwindows.dll) to resolve Qt platform plugin initialization error

--- hooks/hook-PySide6.QtGui.py
+++ hooks/hook-PySide6.QtGui.py
@@ -1,4 +1,23 @@
-# Custom PySide6.QtGui hook override to bypass qmlimportscanner hanging
+import os
+import PySide6
+
+pyside6_dir = os.path.dirname(PySide6.__file__)
+platforms_dir = os.path.join(pyside6_dir, "plugins", "platforms")
+styles_dir = os.path.join(pyside6_dir, "plugins", "styles")
+
 hiddenimports = ['PySide6.QtCore', 'PySide6.QtGui', 'PySide6.QtWidgets']
+
 datas = []
+if os.path.exists(platforms_dir):
+    datas.append((platforms_dir, 'PySide6/plugins/platforms'))
+    datas.append((platforms_dir, 'PySide6/qt6/plugins/platforms'))
+    datas.append((platforms_dir, 'PySide6/qtbase/plugins/platforms'))
+    datas.append((platforms_dir, 'PySide6/Qt/plugins/platforms'))
+    datas.append((platforms_dir, 'platforms'))
+
+if os.path.exists(styles_dir):
+    datas.append((styles_dir, 'PySide6/plugins/styles'))
+    datas.append((styles_dir, 'PySide6/plugins/styles'))
+    datas.append((styles_dir, 'styles'))
+
 binaries = []
